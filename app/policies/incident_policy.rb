c011235ad82110560fe3d1fe0d25ba31d25201d7
Andre Garcia <andrejulio072@hotmail.com>
2022-06-01 00:29:14 +0100

    pundit

--- app/policies/incident_policy.rb
+++ app/policies/incident_policy.rb
@@ -2,10 +2,27 @@ class IncidentPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-    scope.all
+      scope.all
+
+      # For a multi-tenant SaaS app, you may want to use:
+      # scope.where(user: user)
     end
-     def create?
-      return true
-    end
+  end
+  def create?
+    return true
+  end
+
+  def update?
+    record.user == user
+    # - record: the restaurant passed to the `authorize` method in controller
+    # - user:   the `current_user` signed in with Devise.
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+  def show?
+    true
   end
 end
