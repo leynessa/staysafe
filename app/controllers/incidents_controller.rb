0b6287e08f71eabd5b7176c33fc4ccbbd0bdb3d7
Andre Garcia <andrejulio072@hotmail.com>
2022-06-01 00:15:33 +0100

    doing pundit incident

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,16 +1,33 @@
 class IncidentsController < ApplicationController
   def index
+    @incidents = policy_scope(Incident).order(created_at: :desc)
   end
 
-  def show
-  end
+ def show
+  @incident = Incident.find(params[:id])
+  authorize @incident
+ end
 
-  def edit
+
+  def new
+    @incident = Incident.new
+    authorize @incident
   end
 
   def create
+    @incident = Incident.new(incident_params)
+    @incident.user = current_user
+    authorize @incident
+   if @incident.save
+    redirect_to incident_path(@incident)
+   else
+    render :new
+   end
   end
 
-  def new
+  private
+
+  def incident_params
+    params.require(:incident).permit(:name, :description, :photo)
   end
 end
---
+++ app/policies/incident_policy.rb
@@ -0,0 +1,11 @@
+class IncidentPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+    scope.all
+    end
+     def create?
+      return true
+    end
+  end
+end
