0181e9b08cf7735dde6fbe82d1a2bd6733f2be0c
Andre Garcia <andrejulio072@hotmail.com>
2022-06-01 00:22:11 +0100

    incidents controller done

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -25,6 +25,20 @@ class IncidentsController < ApplicationController
    end
   end
 
+   def update
+    @incident = Incident.find(params[:id])
+    @incident.update(params[:incident])
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
+  def destroy
+    @incident = Incident.find(params[:id])
+    @incident.destroy
+
+    # no need for app/views/incidents/destroy.html.erb
+    redirect_to incidents_path
+  end
+
   private
 
   def incident_params
