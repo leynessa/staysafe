2fdc75e65cf8460dd58b257e8fa931417786aecf
Andre Garcia <andrejulio072@hotmail.com>
2022-06-01 10:56:37 +0100

    pundit;

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,4 +1,5 @@
 class IncidentsController < ApplicationController
+  skip_after_action :verify_authorized
   def index
     @incidents = policy_scope(Incident).order(created_at: :desc)
   end
@@ -28,13 +29,14 @@ class IncidentsController < ApplicationController
    def update
     @incident = Incident.find(params[:id])
     @incident.update(params[:incident])
+    authorize @incident
     # Will raise ActiveModel::ForbiddenAttributesError
   end
 
   def destroy
     @incident = Incident.find(params[:id])
     @incident.destroy
-
+    authorize @incident
     # no need for app/views/incidents/destroy.html.erb
     redirect_to incidents_path
   end
