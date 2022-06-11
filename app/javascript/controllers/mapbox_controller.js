c690fd02b7e7ffc7eb4d4d47c5597b35bdbb851b
ashley williams <leynessa1@gmail.com>
2022-06-11 12:55:48 -0300

    search bar

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import mapboxgl from "!mapbox-gl"
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
+
 export default class extends Controller {
   static values = {
     apiKey: String,
@@ -35,4 +36,5 @@ export default class extends Controller {
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
+
 }
