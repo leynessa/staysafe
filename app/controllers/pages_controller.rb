aca29e92a340eeb645f12a5810ca881d5d4bbc71
ashley williams <leynessa1@gmail.com>
2022-06-14 22:02:17 -0300

    secondhome page and logs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home]
+  skip_before_action :authenticate_user!, only: [:home]
   def home
     redirect_to incidents_path if user_signed_in?
   end
