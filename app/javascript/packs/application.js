deb81555e71404f92641f5e6db596550d4a467da
ashley williams <leynessa1@gmail.com>
2022-06-16 00:38:06 -0300

    animation to homepage and sweetalerts added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import { initSweetalert } from '../plugins/init_sweetalert';
 
 Rails.start()
 Turbolinks.start()
@@ -14,3 +15,14 @@ ActiveStorage.start()
 
 import "controllers"
 import "bootstrap"
+
+initSweetalert('#sweet-alert', {
+  title: "Are you sure?",
+  text: "This action cannot be reversed",
+  icon: "warning"
+}, (value) => {
+  if (value) {
+    const link = document.querySelector('#delete-link');
+    link.click();
+  }
+});
